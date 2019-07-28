3f8a621ba2911f4df0adcf2a66ba2011adad60cf
xcf <xcf@qq.com>
2019-07-28 12:41:36 +0800

    branch test

---
+++ app/src/main/java/com/ads/abcbank/xx/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.ads.abcbank.xx.utils;
+
+public class Constants {
+
+    public final static String ROOT_FILE_NAME = "ibcsPlayerData";
+    public final static String COMPRESS_FORMAT = ".png";
+
+    public static final String PDF_CACHE_FILENAME = "PDF_Cache_FileName";
+}
