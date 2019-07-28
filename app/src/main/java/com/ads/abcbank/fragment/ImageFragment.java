78aa1031e14712327e4c973a42cefff3507fac54
dongrh <dongrh@belink.com>
2019-07-28 10:53:18 +0800

    修改logo、修改模板一汇率不刷新的BUG、修改模板二汇率等细节

--- app/src/main/java/com/ads/abcbank/fragment/ImageFragment.java
+++ app/src/main/java/com/ads/abcbank/fragment/ImageFragment.java
@@ -133,7 +133,7 @@ public class ImageFragment extends BaseTempFragment implements View.OnClickListe
     public void setBean(PlaylistBodyBean bean) {
         this.bean = bean;
         initData();
-//        showQRs(bean);
+        showQRs(bean);
     }
 
 
